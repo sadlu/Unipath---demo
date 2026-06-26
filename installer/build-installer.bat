6cf7abdfe17d9b6e4c5a6b81c6bd9ffa6931bbb9
Fouad <fouad@unipath.app>
2026-06-26 11:00:53 +0545

    fix: improve Inno Setup path detection, add debug step

--- installer/build-installer.bat
+++ installer/build-installer.bat
@@ -14,14 +14,24 @@ for %%p in (
   "C:\Program Files\Inno Setup 6\ISCC.exe"
   "C:\Program Files (x86)\Inno Setup 5\ISCC.exe"
   "C:\Program Files\Inno Setup 5\ISCC.exe"
+  "%PROGRAMFILES%\Inno Setup 6\ISCC.exe"
+  "%PROGRAMFILES(X86)%\Inno Setup 6\ISCC.exe"
   "%LOCALAPPDATA%\Programs\Inno Setup 6\ISCC.exe"
+  "%PROGRAMDATA%\chocolatey\lib\innosetup\tools\ISCC.exe"
+  "%ChocolateyInstall%\lib\innosetup\tools\ISCC.exe"
+  "C:\ProgramData\chocolatey\bin\ISCC.exe"
+  "%PROGRAMDATA%\chocolatey\bin\ISCC.exe"
 ) do if exist %%p set "ISCC=%%~p"
 
 if not defined ISCC for /f "delims=" %%a in ('where ISCC.exe 2^>nul') do set "ISCC=%%a"
 
+if not defined ISCC for /r "%PROGRAMDATA%\chocolatey" %%a in (ISCC.exe) do if exist "%%a" set "ISCC=%%a"
+if not defined ISCC for /r "%PROGRAMFILES%" %%a in (ISCC.exe) do if exist "%%a" set "ISCC=%%a"
+if not defined ISCC for /r "%PROGRAMFILES(X86)%" %%a in (ISCC.exe) do if exist "%%a" set "ISCC=%%a"
+
 if not defined ISCC (
   echo ERROR: Inno Setup not found.
-  echo Install from https://jrsoftware.org/isdl.php
+  echo Tried: Chocolatey, Program Files, Program Files (x86), Local AppData
   exit /b 1
 )
 
