716434f237f187f903da36c73b6b2643eccbf2ac
Fouad <fouad@unipath.app>
2026-06-26 09:13:37 +0545

    fix: remove deprecated nativeWindowOpen Electron option

--- electron/main.ts
+++ electron/main.ts
@@ -307,7 +307,6 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
       nodeIntegration: false,
-      nativeWindowOpen: true,
     },
   })
 
